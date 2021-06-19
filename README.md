9e413d7209216b9afc6efe0a32494771bf330193
Robert <robert.pastor0691@orange.fr>
2021-06-19 22:32:27 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# AzureCSharpRestAPI
-Azure CSharp Rest API
+# Contributing
+
+This project has adopted the [Microsoft Open Source Code of Conduct](https://opensource.microsoft.com/codeofconduct/). 
+For more information see the [Code of Conduct FAQ](https://opensource.microsoft.com/codeofconduct/faq/) 
+or contact [opencode@microsoft.com](mailto:opencode@microsoft.com) with any additional questions or comments.
+
+To run this project, first fill in your storage account name in Program.cs.
+You will need to provide the storage account Access Keys
+
+The sample will list the containers in the storage account.
+For each container, it will list the blobs.
---
+++ StorageRestApiAuth/Program.cs
@@ -0,0 +1,166 @@
+﻿namespace StorageRestApiAuth
+{
+    using System;
+    using System.Globalization;
+    using System.Net;
+    using System.Net.Http;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Xml.Linq;
+
+    internal static class Program
+    {
+        static string StorageAccountName = "amsairdatastorageaccount";
+        static string StorageAccountKey = "";
+        
+        private static void Main()
+        {
+
+            // use ReadLine() to read the entered line
+            Console.WriteLine("In the Storage Account, use left hand side menu to get one Access Key");
+            Console.WriteLine("In the Storage Account, click overview to ensure that Storage account key access is Enabled");
+            Console.WriteLine("Please enter the Storage Account Access Key");
+            StorageAccountKey = Console.ReadLine();
+
+            // display the line
+            Console.WriteLine("Storage Account Access Key = {0}", StorageAccountKey);
+
+            // List the containers in a storage account.
+            ListContainersAsyncREST(StorageAccountName, StorageAccountKey, CancellationToken.None).GetAwaiter().GetResult();
+
+            Console.WriteLine("It is finished -> Press any key to end the program.");
+            Console.ReadLine();
+        }
+
+
+        private static async Task ListContainerContentAsyncREST(string storageAccountName, string containerName, string storageAccountKey, CancellationToken cancellationToken)
+        {
+
+            // Construct the URI. This will look like this:
+            //   https://myaccount.blob.core.windows.net/resource
+            //   https://docs.microsoft.com/en-us/rest/api/storageservices/list-blobs
+            String uri = string.Format("https://{0}.blob.core.windows.net/{1}/?restype=container&comp=list", storageAccountName, containerName);
+
+            Console.WriteLine(uri);
+
+            // Set this to whatever payload you desire. Ours is null because 
+            //   we're not passing anything in.
+            Byte[] requestPayload = null;
+
+            //specify to use TLS 1.2 as default connection
+            System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            //Instantiate the request message with a null payload.
+            using (var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, uri)
+            { Content = (requestPayload == null) ? null : new ByteArrayContent(requestPayload) })
+            {
+
+                // Add the request headers for x-ms-date and x-ms-version.
+                DateTime now = DateTime.UtcNow;
+                httpRequestMessage.Headers.Add("x-ms-date", now.ToString("R", CultureInfo.InvariantCulture));
+                httpRequestMessage.Headers.Add("x-ms-version", "2009-09-19");
+                // If you need any additional headers, add them here before creating
+                //   the authorization header. 
+
+                // Add the authorization header.
+                httpRequestMessage.Headers.Authorization = AzureStorageAuthenticationHelper.GetAuthorizationHeader(
+                   storageAccountName, storageAccountKey, now, httpRequestMessage);
+
+                // Send the request.
+                using (HttpResponseMessage httpResponseMessage = await new HttpClient().SendAsync(httpRequestMessage, cancellationToken))
+                {
+                    // If successful (status code = 200), 
+                    //   parse the XML response for the container names.
+                    if (httpResponseMessage.StatusCode == HttpStatusCode.OK)
+                    {
+                        Console.WriteLine(httpResponseMessage.ToString());
+
+                        String xmlString = await httpResponseMessage.Content.ReadAsStringAsync();
+                        XElement x = XElement.Parse(xmlString);
+                        foreach (XElement blob in x.Element("Blobs").Elements("Blob"))
+                        {
+                            Console.WriteLine("Blob name = {0}", blob.Element("Name").Value);
+                            Console.WriteLine("Blob Last Modified = {0}", blob.Element("Properties").Element("Last-Modified").Value);
+                            Console.WriteLine("Blob Content-Length = {0}", blob.Element("Properties").Element("Content-Length").Value);
+                            Console.WriteLine("-----------------");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine(httpResponseMessage.ToString());
+                        Console.WriteLine("Status code = {0}", httpResponseMessage.StatusCode.ToString());
+                    }
+                }
+            }
+
+        }
+
+            /// <summary>
+            /// This is the method to call the REST API to retrieve a list of
+            /// containers in the specific storage account.
+            /// This will call CreateRESTRequest to create the request, 
+            /// then check the returned status code. If it's OK (200), it will 
+            /// parse the response and show the list of containers found.
+            /// </summary>
+            private static async Task ListContainersAsyncREST(string storageAccountName, string storageAccountKey, CancellationToken cancellationToken)
+        {
+
+            // Construct the URI. This will look like this:
+            //   https://myaccount.blob.core.windows.net/resource
+            String uri = string.Format("https://{0}.blob.core.windows.net/?comp=list", storageAccountName);
+
+            Console.WriteLine(uri);
+
+            // Set this to whatever payload you desire. Ours is null because 
+            //   we're not passing anything in.
+            Byte[] requestPayload = null;
+
+            //specify to use TLS 1.2 as default connection
+            System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            //Instantiate the request message with a null payload.
+            using (var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, uri)
+            { Content = (requestPayload == null) ? null : new ByteArrayContent(requestPayload) })
+            {
+
+                // Add the request headers for x-ms-date and x-ms-version.
+                DateTime now = DateTime.UtcNow;
+                httpRequestMessage.Headers.Add("x-ms-date", now.ToString("R", CultureInfo.InvariantCulture));
+                httpRequestMessage.Headers.Add("x-ms-version", "2017-04-17");
+                // If you need any additional headers, add them here before creating
+                //   the authorization header. 
+
+                // Add the authorization header.
+                httpRequestMessage.Headers.Authorization = AzureStorageAuthenticationHelper.GetAuthorizationHeader(
+                   storageAccountName, storageAccountKey, now, httpRequestMessage);
+
+                // Send the request.
+                using (HttpResponseMessage httpResponseMessage = await new HttpClient().SendAsync(httpRequestMessage, cancellationToken))
+                {
+                    // If successful (status code = 200), 
+                    //   parse the XML response for the container names.
+                    if (httpResponseMessage.StatusCode == HttpStatusCode.OK)
+                    {
+                        Console.WriteLine(httpResponseMessage.ToString());
+
+                        String xmlString = await httpResponseMessage.Content.ReadAsStringAsync();
+                        XElement x = XElement.Parse(xmlString);
+                        foreach (XElement container in x.Element("Containers").Elements("Container"))
+                        {
+                            Console.WriteLine("Container name = {0}", container.Element("Name").Value);
+
+                            // List blobs in a container
+                            string containerName = container.Element("Name").Value;
+                            ListContainerContentAsyncREST(StorageAccountName, containerName, StorageAccountKey, CancellationToken.None).GetAwaiter().GetResult();
+
+                        }
+                    } else
+                    {
+                        Console.WriteLine(httpResponseMessage.ToString());
+                        Console.WriteLine("Status code = {0}" , httpResponseMessage.StatusCode.ToString());
+                    }
+                }
+            }
+        }
+    }
+}
